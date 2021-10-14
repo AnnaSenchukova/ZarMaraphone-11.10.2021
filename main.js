5378d173d384498a2d393259c1ff35147f596bf6
anna <anna.senchukova@goods.ru>
2021-10-14 19:11:54 +0300

    zarmaraphone2021-homework-2: добавлена функция createPlayer отрисовывающая блок с игроком в dom

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const player1 = {
     name: 'Sonya',
-    hp: 200,
+    hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
     weapon: ['stranglehold', 'guns', 'fan', 'knife','club'],
     attack: function(){
@@ -10,7 +10,7 @@ const player1 = {
 
 const player2 = {
     name: 'Kitana',
-    hp: 200,
+    hp: 50,
     img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
     weapon: ['stranglehold', 'guns', 'fan', 'knife','club'],
     attack: function(){
@@ -18,5 +18,47 @@ const player2 = {
     }
 };
 
+function createPlayer(player, namePlayer, imagePlayer, hpPlayer) {
+    const arenasHtmlBlock = document.querySelector('.arenas');
+
+    const playerHtmlBlock = document.createElement('div');
+    playerHtmlBlock.classList.add(player);
+
+    const playerProgressbarHtmlBlock = document.createElement('div');
+    playerProgressbarHtmlBlock.classList.add('progressbar');
+
+    const playerLifeHtml = document.createElement('p');
+    playerLifeHtml.classList.add('life');
+    playerLifeHtml.innerText = hpPlayer;
+    playerLifeHtml.style.width = '100%';
+    playerLifeHtml.style.color = 'white';
+    playerLifeHtml.style.fontSize = '20px';
+    playerLifeHtml.style.paddingLeft = '10px';
+
+    const playerNameHtml = document.createElement('p');
+    playerNameHtml.classList.add('name');
+    playerNameHtml.innerText = namePlayer;
+    playerNameHtml.style.margin = '0';
+
+
+    const playerCharacterHtmlBlock = document.createElement('div');
+    playerCharacterHtmlBlock.classList.add('character');
+
+    const playerImageHtml = document.createElement('img');
+    playerImageHtml.src = imagePlayer;
+
+    arenasHtmlBlock.appendChild(playerHtmlBlock);
+
+    playerHtmlBlock.appendChild(playerProgressbarHtmlBlock);
+    playerHtmlBlock.appendChild(playerCharacterHtmlBlock);
+
+    playerProgressbarHtmlBlock.appendChild(playerNameHtml);
+    playerProgressbarHtmlBlock.appendChild(playerLifeHtml);
+
+    playerCharacterHtmlBlock.appendChild(playerImageHtml)
+}
+
 player1.attack();
 player2.attack();
+createPlayer('player1', player1.name, player1.img, player1.hp);
+createPlayer('player2', player2.name, player2.img, player2.hp);
